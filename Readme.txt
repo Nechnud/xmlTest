9987f615ef90b03a3a02aa736d5d32c2d5b58c95
Nehcnud <nenaddjukin@gmail.com>
2023-04-18 23:55:31 +0200

    Final

---
+++ Readme.txt
@@ -0,0 +1,2 @@
+1. write npm i xml2js in your terminal to intall xml2js package.
+2. write node index in your terminal to run the code.
---
+++ index.js
@@ -0,0 +1,20 @@
+const { error } = require("console");
+const fs = require("fs");
+const xml2js = require("xml2js");
+
+// Parse XML data and finds id
+function findIdXml(id) {
+  xml2js.parseString(xml, (err, result) => {
+    if (err) throw err;
+
+    const xml = fs.readFileSync("sma_gentext.xml");
+
+    const value = result.root.file[0].body[0]["trans-unit"].find(
+      (unit) => unit.$.id === id
+    );
+
+    const data = JSON.stringify({ target: value.target[0] });
+    fs.writeFileSync("result.json", data);
+  });
+}
+findIdXml("42007");
